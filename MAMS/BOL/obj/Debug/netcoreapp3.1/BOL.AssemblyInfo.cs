98f4788e935ddff0515ceb4232dfdb5c8b576a48
Muhammad Usman <m.usman@outsourcenz.com>
2024-05-28 07:48:25 +0500

    Merged changes

--- MAMS/BOL/obj/Debug/netcoreapp3.1/BOL.AssemblyInfo.cs
+++ MAMS/BOL/obj/Debug/netcoreapp3.1/BOL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BOL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2362f47bcb717bc808595317d074aa2f97693b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BOL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BOL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
